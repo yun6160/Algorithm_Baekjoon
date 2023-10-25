6db6bd766c6bd31198b6a6bd39b223db71a25bf4
yun6160 <devyun6160@gmail.com>
2023-10-26 03:41:24 +0900

    first commit

---
+++ baekjoon/1152.js
@@ -0,0 +1,8 @@
+let input = require(`fs`).readFileSync(`./input.txt`).toString().split(` `);
+//let input = require(`fs`).readFileSync(`/dev/stdin`).toString().split(` `);
+function solution(input) {
+  let answer = 0;
+  console.log(input);
+  return answer;
+}
+console.log(solution(input));
