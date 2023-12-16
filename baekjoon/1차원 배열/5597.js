b86731ffc6b012ed2a1f96219308f12563024d17
yun6160 <devyun6160@gmail.com>
2023-12-17 00:48:48 +0900

    백준 1차원 배열 5597 추가

---
+++ baekjoon/1차원 배열/5597.js
@@ -0,0 +1,21 @@
+let fs = require(`fs`);
+const filePath = process.platform === `linux` ? `/dev/stdin` : `./input.txt`;
+// 문자 하나만 입력받을 경우
+// let input = fs.readFileSync(filePath).toString().trim();
+// 한칸 띄어쓰기로 구분
+// input[0], input[1] 배열에서 꺼내쓰면 된다.
+// let input = fs.readFileSync(filePath).toString().split(` `)
+// 줄바꿈으로 구분
+let input = fs.readFileSync(filePath).toString().trim().split(`\n`);
+// 만약 인풋값이 숫자라면
+//let input = fs.readFileSync(filePath).toString().split(` `).map(function(a) {return +a})
+function solution(input) {
+    let answer = "";
+    const newArr = new Array(30).fill("X");
+    input.forEach((v) => (newArr[v - 1] = "O"));
+    newArr.forEach((v, i) => {
+        if (v == "X") answer += i + 1 + "\n";
+    });
+    return answer;
+}
+console.log(solution(input));
