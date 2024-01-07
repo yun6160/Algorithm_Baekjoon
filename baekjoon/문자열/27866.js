2a6cc35bf8b9e0c393e5045e767606ae58722e58
yun6160 <devyun6160@gmail.com>
2024-01-07 14:34:29 +0900

    백준 문자열 4문제 풀이 완

---
+++ baekjoon/문자열/27866.js
@@ -0,0 +1,17 @@
+let fs = require(`fs`);
+const filePath = process.platform === `linux` ? `/dev/stdin` : `./input.txt`;
+// 문자 하나만 입력받을 경우
+// let input = fs.readFileSync(filePath).toString().trim()
+// 한칸 띄어쓰기로 구분
+// input[0], input[1] 배열에서 꺼내쓰면 된다.
+// let input = fs.readFileSync(filePath).toString().split(` `)
+// 줄바꿈으로 구분
+let input = fs.readFileSync(filePath).toString().trim().split(`\n`);
+// 만약 인풋값이 숫자라면
+//let input = fs.readFileSync(filePath).toString().split(` `).map(function(a) {return +a})
+function solution(input) {
+    const str = input[0];
+    const index = input[1] - 1;
+    return str[index];
+}
+console.log(solution(input));
