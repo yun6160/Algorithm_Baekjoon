84561d46f8df28e2065e289eb7f3c91f1fab1e50
yun6160 <devyun6160@gmail.com>
2024-01-07 14:39:38 +0900

    remove: 프로그래머스 문제

--- programmers/짝수와 홀수.js
+++
@@ -1,4 +0,0 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/12937
-function solution(num) {
-  return num % 2 == 0 ? "Even" : "Odd";
-}
--- programmers/하샤드 수.js
+++
@@ -1,7 +0,0 @@
-function solution(x) {
-  const xSum = x
-    .toString()
-    .split("")
-    .reduce((a, b) => Number(a) + Number(b));
-  return x % xSum == 0;
-}
