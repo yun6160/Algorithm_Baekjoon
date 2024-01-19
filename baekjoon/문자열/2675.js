15d75cbe472feeb27899d41078120ac1fe4ec165
yun6160 <devyun6160@gmail.com>
2024-01-19 23:34:53 +0900

    백준 문자열 2675 풀이 완

---
+++ baekjoon/문자열/2675.js
@@ -0,0 +1,25 @@
+let fs = require(`fs`);
+const filePath = process.platform === `linux` ? `/dev/stdin` : `./input.txt`;
+// 문자 하나만 입력받을 경우
+// let input = fs.readFileSync(filePath).toString().trim();
+// 한칸 띄어쓰기로 구분 nput[0], input[1] 배열에서 꺼내쓰면 된다.
+// let input = fs.readFileSync(filePath).toString().split(` `)
+// 줄바꿈으로 구분
+let input = fs.readFileSync(filePath).toString().trim().split(`\n`);
+// 만약 인풋값이 숫자라면
+//let input = fs.readFileSync(filePath).toString().split(` `).map(function(a) {return +a})
+function solution(input) {
+    let answer = "";
+    for (let i = 1; i <= input[0]; i++) {
+        let [index, str] = input[i].split(" ");
+        let strArr = [...str];
+        strArr.forEach((v) => {
+            for (let j = 0; j < index; j++) {
+                answer += v;
+            }
+        });
+        answer += "\n";
+    }
+    return answer;
+}
+console.log(solution(input));
