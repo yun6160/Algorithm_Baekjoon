f920d78a45b1312a43bfec28dfb4720ce9105c89
yun6160 <devyun6160@gmail.com>
2024-02-04 01:22:02 +0900

    심화 1 2941 완료

---
+++ baekjoon/심화 1/2941.js
@@ -0,0 +1,19 @@
+let fs = require(`fs`);
+const filePath = process.platform === `linux` ? `/dev/stdin` : `./input.txt`;
+// 문자 하나만 입력받을 경우
+let input = fs.readFileSync(filePath).toString().trim();
+// 한칸 띄어쓰기로 구분 input[0], input[1] 배열에서 꺼내쓰면 된다.
+// let input = fs.readFileSync(filePath).toString().split(` `)
+// 줄바꿈으로 구분
+//let input = fs.readFileSync(filePath).toString().trim().split(`\n`)
+// 만약 인풋값이 숫자라면
+//let input = fs.readFileSync(filePath).toString().split(` `).map(function(a) {return +a})
+function solution(input) {
+    let answer = input;
+    const croaAlphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+    for (let str of croaAlphabet) {
+        answer = answer.replaceAll(str, "A");
+    }
+    return answer.length;
+}
+console.log(solution(input));
