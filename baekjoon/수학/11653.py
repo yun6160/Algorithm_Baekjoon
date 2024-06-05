59e617dba3e771f19a3780fa41d0648149ddfb41
yun6160 <devyun6160@gmail.com>
2024-06-06 03:35:44 +0900

    수학 solve

---
+++ baekjoon/수학/11653.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+# 한 줄을 읽어서 정수로 변환
+# N = int(input())
+# print(N)
+# 한 줄을 읽고 공백으로 구분된 문자를 배열로 
+# print(input().split())
+# 한 줄을 읽고 공백으로 구분된 문자를 정수로 변환한 배열
+# print(list(map(int, input().split())))
+# N행으로 이루어진 2차원 배열 입력
+# N = int(input())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+# print(arr)
+answer = ''
+N = int(input())
+
+
+i = 2
+while N != 1:
+    if N % i == 0:
+        answer += str(i) + '\n'
+        N //= i
+    else:
+        i += 1
+
+print(answer)
